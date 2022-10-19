4854def7d0709fa6481650a9f78002e45a3717da
jacob <jacob.dekok01@gmail.com>
2022-10-19 13:41:17 -0700

    Peer Review Recommendations

--- Program.cs
+++ Program.cs
@@ -12,10 +12,31 @@ string playAgain = "YES";
 
 while(playAgain == "YES"){
 
+    bool nameIsNumber = false;
+    bool timeIsNumber = false;
+    string wakeUpTime = " ";
+
 Console.WriteLine("What is your name?");
 string yourName = Console.ReadLine();
-Console.WriteLine("Hello " + yourName + ", just out of curiosity what time did you wake up this morning?");
-string wakeUpTime = Console.ReadLine();
+
+    nameIsNumber = Int32.TryParse(yourName, out int numberName);
+
+if(nameIsNumber == true){
+    Console.WriteLine("Did your Mom and Dad really name you after a number?");
+}
+
+while(timeIsNumber == false)
+{
+    Console.WriteLine(yourName + ", just out of curiosity what time did you wake up this morning?");
+    wakeUpTime = Console.ReadLine();
+
+    timeIsNumber = Int32.TryParse(wakeUpTime, out int numberTime);
+
+    if(timeIsNumber == false){
+    Console.WriteLine("That wasn't really a time, I'll ask again. ");
+}
+}
+
 Console.WriteLine(yourName + " woke up at " + wakeUpTime + " today.");
 
 Console.WriteLine("Would you like to give me another name and time? 'YES' 'NO'");
